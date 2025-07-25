7c32c2d682db96bc6f9f4d931cae81c304f18e80
sirsha <sirshathapa02@gmail.com>
2025-07-25 16:23:40 +0000

    Jenkinsfile updated

--- Automations/updatefrontendnew.sh
+++ Automations/updatefrontendnew.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Set the Instance ID and path to the .env file
-INSTANCE_ID="i-0408d287f1754b77c"
+INSTANCE_ID="i-0f2c3c9b720b1923c"
 
 # Retrieve the public IP address of the specified EC2 instance
 ipv4_address=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
